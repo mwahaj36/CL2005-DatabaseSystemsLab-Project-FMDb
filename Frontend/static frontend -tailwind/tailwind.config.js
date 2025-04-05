b333d68f0fe27cb00b51d861fb21704859032970
mwahaj36 <mwahaj25@gmail.com>
2025-04-05 07:07:38 +0500

    alot of shit

--- Frontend/static frontend -tailwind/tailwind.config.js
+++ Frontend/static frontend -tailwind/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         purpleWhite: 'hsl(320, 34%, 93%)',
         purple: 'hsl(297.7358490566038, 21.372549019607842, 48.62385321100917)',
         darkPurple: 'hsl(267, 50.91%, 13.33%)',
+        gold: 'hsl(45, 100%, 50%)',
+        emeraldGreen: 'hsl(140, 52%, 55%)',
 
       }
     },
