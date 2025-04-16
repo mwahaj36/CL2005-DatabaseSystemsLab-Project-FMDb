c9f4ea96e7251ce4669629fd472c844315076f0d
Ibrahim-Alam-3075 <ibrahimalam545@gmail.com>
2025-04-16 17:30:42 +0500

    Added SQL Queries File For_Love

---
+++ Backend/FOR_Love/Queries.sql
@@ -0,0 +1,91 @@
+-- TO GET 5 most recent movies watched/logged by friends
+
+DECLARE @CurrentUserID INT = 1;  -- Replace with the actual user ID
+
+SELECT TOP 5 M.MovieID, M.Title, M.MoviePosterLink
+FROM Activity A
+JOIN Users U ON A.UserID = U.UserID
+JOIN Movies M ON A.MovieID = M.MovieID
+WHERE A.UserID IN (
+    SELECT User2ID FROM Friends WHERE User1ID = @CurrentUserID
+    UNION
+    SELECT User1ID FROM Friends WHERE User2ID = @CurrentUserID
+)
+ORDER BY A.ActivityDateTime DESC;
+
+
+
+-- 5 most recent movies that your friends added to their watchlists (from the UserWatchlist table).
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT TOP 5 M.MovieID, M.Title, M.MoviePosterLink
+FROM UserWatchlist UW
+JOIN Users U ON UW.UserID = U.UserID
+JOIN Movies M ON UW.MovieID = M.MovieID
+WHERE UW.UserID IN (
+    SELECT User2ID FROM Friends WHERE User1ID = @CurrentUserID
+    UNION
+    SELECT User1ID FROM Friends WHERE User2ID = @CurrentUserID
+)
+ORDER BY UW.AddedAt DESC;
+
+
+-- 5 most recent activities of a user’s friends 
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT TOP 5 A.ActivityID, A.ActivityDateTime, A.IsLogged, A.UserID, A.MovieID, A.Ratings, A.Review, A.IsReply
+FROM Activity A
+JOIN Users U ON A.UserID = U.UserID
+JOIN Movies M ON A.MovieID = M.MovieID
+WHERE A.UserID IN (
+    SELECT User2ID FROM Friends WHERE User1ID = @CurrentUserID
+    UNION
+    SELECT User1ID FROM Friends WHERE User2ID = @CurrentUserID
+)
+ORDER BY A.ActivityDateTime DESC;
+
+
+
+-- Count of unique MovieIDs in the Activity table for a specific user:
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT COUNT(DISTINCT MovieID) AS UniqueMoviesAdded
+FROM Activity
+WHERE UserID = @CurrentUserID;
+
+
+
+-- count of movies logged by a specific user 
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT COUNT(MovieID) AS LoggedMoviesCount
+FROM Activity
+WHERE UserID = @CurrentUserID AND IsLogged = 1;
+
+
+
+-- Count of movies a user has liked,
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT COUNT(*) AS LikedMoviesCount
+FROM UserLikedMovies
+WHERE UserID = @CurrentUserID;
+
+
+
+--  First favorite movie backdrop of the user 
+
+DECLARE @CurrentUserID INT = 1; -- Replace with the actual user ID
+
+SELECT TOP 1 M.MovieBackdropLink
+FROM UserFavorites UF
+JOIN Movies M ON UF.MovieID = M.MovieID
+WHERE UF.UserID = @CurrentUserID
+ORDER BY UF.Rank ASC;
+
+
