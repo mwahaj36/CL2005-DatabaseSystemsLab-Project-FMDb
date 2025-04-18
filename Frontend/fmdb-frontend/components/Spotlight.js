1fbdd1902a0421e904e8ee7427f47a60e145374f
Muhammad Wahaj <137283515+mwahaj36@users.noreply.github.com>
2025-04-18 15:00:54 +0500

    made it prettier uwu

--- Frontend/fmdb-frontend/components/Spotlight.js
+++ Frontend/fmdb-frontend/components/Spotlight.js
@@ -4,7 +4,7 @@ import MovieCard from "./MovieCard"; // Adjust the path if necessary
 const Spotlight = ({ movies }) => {
   return (
     <section id="Spotlight" className="relative z-10">
-      <div className="max-w-8xl px-5 mx-auto mt-20 text-center">
+      <div className="max-w-8xl px-5 mx-auto mt-10 text-center">
         <div className="flex flex-col  md:flex-row md:space-x-6 space-y-20 md:space-y-0">
           {/* Dynamically render each movie using MovieCard */}
           {movies.map((movie, index) => (
